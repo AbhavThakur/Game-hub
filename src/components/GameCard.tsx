e146b2fea6e09cd57bbef4f25bc90b257c1bef6e
Abhav Thakur <abhav.thakur2018@vitstudent.ac.in>
2023-09-10 15:00:39 +0530

    Displaying critic score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,6 @@
-import { Card, CardBody, Heading, Image } from '@chakra-ui/react';
+import { Card, CardBody, HStack, Heading, Image } from '@chakra-ui/react';
 import { Game } from '../hooks/useGames';
+import CriticScore from './CriticScore';
 import PlatformIconList from './PlatformIconList';
 
 interface Props {
@@ -11,10 +12,15 @@ function GameCard({ Game }: Props) {
     <Card borderRadius={10} overflow={'hidden'}>
       <Image src={Game.background_image} />
       <CardBody>
-        <Heading fontSize={'2xl'}>{Game.name}</Heading>
-        <PlatformIconList
-          Platform={Game.parent_platforms.map((platform) => platform.platform)}
-        />
+        <Heading fontSize="2xl">{Game.name}</Heading>
+        <HStack justifyContent="space-between">
+          <PlatformIconList
+            Platform={Game.parent_platforms.map(
+              (platform) => platform.platform
+            )}
+          />
+          <CriticScore score={Game.metacritic} />
+        </HStack>
       </CardBody>
     </Card>
   );
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -16,6 +16,7 @@ export interface Game {
       platform: Platform;
     }
   ];
+  metacritic: number;
 }
 
 interface FetchGameResponse {
