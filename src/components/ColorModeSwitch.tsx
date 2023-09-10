dceb224852f68a6ec0536179d2518a7e36a957f5
Abhav Thakur <abhav.thakur2018@vitstudent.ac.in>
2023-09-11 00:14:13 +0530

    added github link

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,4 +1,11 @@
-import { HStack, Switch, Text, useColorMode } from '@chakra-ui/react';
+import {
+  HStack,
+  IconButton,
+  Switch,
+  Text,
+  useColorMode,
+} from '@chakra-ui/react';
+import { BsGithub } from 'react-icons/bs';
 
 function ColorModeSwitch() {
   const { colorMode, toggleColorMode } = useColorMode();
@@ -6,6 +13,15 @@ function ColorModeSwitch() {
     <HStack>
       <Switch colorScheme="green" onChange={toggleColorMode} />
       <Text>{colorMode.charAt(0).toUpperCase() + colorMode.slice(1)} Mode</Text>
+
+      <a
+        target="_blank"
+        rel="noopener"
+        href="https://github.com/AbhavThakur"
+        aria-label="GitHub"
+      >
+        <IconButton aria-label="Github" icon={<BsGithub />} />
+      </a>
     </HStack>
   );
 }
